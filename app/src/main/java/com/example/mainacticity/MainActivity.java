ade8476bb0921fd5617b78c7636a2d4aee0ccc67
Arc-husky <451953138@qq.com>
2021-07-19 23:08:25 +0800

    更新主界面

--- app/src/main/java/com/example/mainacticity/MainActivity.java
+++ app/src/main/java/com/example/mainacticity/MainActivity.java
@@ -36,12 +36,13 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
 //         Passing each menu ID as a set of Ids because each
 //         menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_notifications, R.id.navigation_mine)
-                .build();
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.navigation_home, R.id.navigation_notifications, R.id.navigation_mine)
+//                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_upload);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
     }
 
     public void changeStatusBarTextColor(Window window, boolean isBlack) {
